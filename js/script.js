5d43348719f7da2fe2cfe74e0dd39887f97d8b1a
Robert <lum.robert@gmail.com>
2014-06-26 20:17:33 -0400

    instagram template set up for modal

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-$(function($){
-	$('.popBoxLink').click(function(e){
-		var standardLink = $(this).find('span').html();
+jQuery(function($){
+	console.log('script.js ran')
+	$('body').on('click', '.instagram-img', function(e){
+		var standardLink = $(this).next().find('span').html();
 		console.log(standardLink);
-		alert('hello')
+		return false;
 	});
 });
+
+// <div class='instagram-container col-sm-3'>
+// 	<a href='#' class='popBoxLink' id='popBox" + i + "'>
+// 		<img class='instagram-img' src='" + lowRes + "'/>
+
+// 		<div class='hide-content'>
+// 			<p>" + caption + "</p>
+// 			<span>" + standardRes + "</span>
+// 		</div>
+// 	</a>
+// </div>

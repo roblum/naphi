5eda43c4b00e9902852d86c19ba9178b0f659c26
Robert <lum.robert@gmail.com>
2014-06-27 00:02:34 -0400

    implemented image cache of instagram feed

--- js/instagram.script.js
+++ js/instagram.script.js
@@ -1,6 +1,6 @@
 function pullFeed() {
 	var parms = {
-		count: '9'
+		count: '8'
 		,client_id: 'df70d4f39d3649a9b724876a0f2de343'
 	}
 
@@ -14,12 +14,24 @@ function pullFeed() {
 		,success: function(res){
 			console.log(res)
 			var links = res.data;
+				
+				//Create arrays for image caching
+				preloadImagesList = [];
+				var imageURLs = [];
 
-			for(i=1;i<links.length;i++) {
+			for(i=0;i<links.length;i++) {
 				var lead = links[i]
 					,lowRes = lead.images.low_resolution.url
 					,standardRes = lead.images.standard_resolution.url
 					,caption = lead.caption.text;
+
+						//Create new images from instagram feed and push to array
+						imageURLs.push(standardRes);
+
+						var img = new Image();
+					        img.src = imageURLs[i];
+					        preloadImagesList.push(img);
+
 
 					$("#instagram-feed").append("<div class='instagram-container col-sm-3'><a href='#' class='popBoxLink' id='popBox" + i + "'><img class='instagram-img' src='" + lowRes + "'/><div class='hide-content'><p>" + caption + "</p><span>" + standardRes + "</span></div></a></div>");
 

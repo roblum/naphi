18eb3706668c5f01cffe4f70adaf6abcdcae4d24
Robert <lum.robert@gmail.com>
2014-10-11 16:52:36 -0400

    put chapters in separate js file

---
+++ script/chapter-info.js
@@ -0,0 +1,38 @@
+var chapterInfo = {
+	alpha : {
+          icon : 'images/alpha.png'
+          ,geo : new google.maps.LatLng(42.6850794,-73.826036)
+     },
+     beta : {
+          icon : 'images/beta.png'
+          ,geo : new google.maps.LatLng(40.739995,-73.983642)
+     },
+     gamma : {
+          icon : 'images/gamma.png'
+          ,geo : new google.maps.LatLng(40.913096,-73.123586)
+     },
+     delta : {
+          icon : 'images/delta.png'
+          ,geo : new google.maps.LatLng(43.000809,-78.78897)
+     },
+     epsilon : {
+        	icon : 'images/epsilon.png'
+        	,geo : new google.maps.LatLng(40.694074, -73.986932)
+     },
+     zeta : {
+          icon : 'images/zeta.png'
+          ,geo : new google.maps.LatLng(42.088828,-75.966972)
+     },
+     eta : {
+          icon : 'images/eta.png'
+          ,geo : new google.maps.LatLng(43.0383461,-76.134901)
+     },
+     theta : {
+          icon : 'images/theta.png'
+          ,geo : new google.maps.LatLng(40.744323,-74.025868)
+     },
+     iota : {
+          icon : 'images/iota.png'
+          ,geo : new google.maps.LatLng(40.4846831,-74.4366424)
+     }
+}

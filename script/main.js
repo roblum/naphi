e7f80ee75333a523a88381e9628dcb65a686ebeb
Robert <lum.robert@gmail.com>
2014-10-11 16:55:53 -0400

    setting up rich marker creation and image caching

--- script/main.js
+++ script/main.js
@@ -1,12 +1,35 @@
 //(function(){
-var myLatlng = new google.maps.LatLng(40.71043855741909, -74.00503814701665),
-    mapOptions = {
+var myLatlng = new google.maps.LatLng(40.71043855741909, -74.00503814701665)
+    ,mapOptions = {
         zoom: 14,
         center: myLatlng,
         mapTypeControl: false,
         styles: googleMapStyles
-      },
-    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions),
-    currentZoom = map.getZoom();
+    }
+    ,map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions)
+    ,currentZoom = map.getZoom()
+    ,imageCacheArray = [];
 
+
+$('body').on('click', '.chapter-navigation li', function(){
+    var current = this.id
+        ,cMarker = new RichMarker({
+            position: geo
+            ,map: map
+            ,content: '<div class="ugc-content" id="' + ugc + '"><img src="' + geoStore[ugc].small_image + '"></div>'
+        });
+
+        cMarker.setMap(map);
+    map.panTo(coord);
+});
+
+
+    function imageCache(asset){
+        var imageCacheObj = {};
+
+            imageCacheObj.cache = new Image();
+            imageCacheObj.cache.src = asset;
+
+        imageCacheArray.push(imageCacheObj);
+    }
 // })();

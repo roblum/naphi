d3b4937be1a909afe98316e1fb029ae577546ed4
Robert <lum.robert@gmail.com>
2014-10-11 23:23:07 -0400

    moved marker creation to function; initiate only epsilon on page load

--- script/main.js
+++ script/main.js
@@ -10,19 +10,24 @@ var myLatlng = new google.maps.LatLng(40.71043855741909, -74.00503814701665)
     ,currentZoom = map.getZoom()
     ,imageCacheArray = [];
 
+createRichMarker('epsilon');
 
 $('body').on('click', '.chapter-navigation li', function(){
-    var current = $(this).data('chapter')
-        ,cMarker = new RichMarker({
-            position: chapterInfo[current].geo
+    var current = $(this).data('chapter');
+    
+    createRichMarker(current);
+    map.panTo(chapterInfo[current].geo);
+});
+
+function createRichMarker(chapter){
+    var cMarker = new RichMarker({
+            position: chapterInfo[chapter].geo
             ,map: map
             ,content: '<div>Testing</div>'
         });
 
-        cMarker.setMap(map);
-    map.panTo(cMarker.position);
-});
-
+    cMarker.setMap(map);
+}
 
     function imageCache(asset){
         var imageCacheObj = {};

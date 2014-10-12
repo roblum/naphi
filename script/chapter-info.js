127c20f06392765768bf50aa571c7d39f10b0cfb
Robert <lum.robert@gmail.com>
2014-10-12 03:22:02 -0400

    added circle to epsilon chapter object for testing css3 spin animation; working

--- script/chapter-info.js
+++ script/chapter-info.js
@@ -18,6 +18,7 @@ var chapterInfo = {
      epsilon : {
         	icon : 'images/epsilon.png'
         	,geo : new google.maps.LatLng(40.694074, -73.986932)
+          ,content : '<div class="chapter-marker" id="epsilon-marker"><img src="images/nyupoly_circle_1px.png" /></div>'
      },
      zeta : {
           icon : 'images/zeta.png'

222664fb5f4b9f34b8c31e8efd876a5cf73154c5
Robert <lum.robert@gmail.com>
2014-10-13 00:09:56 -0400

    new lat,long for desktop view; displays along side content box

--- script/chapter-info.js
+++ script/chapter-info.js
@@ -2,38 +2,47 @@ var chapterInfo = {
 	alpha : {
           icon : 'images/alpha.png'
           ,geo : new google.maps.LatLng(42.6850794,-73.826036)
+          ,selection : new google.maps.LatLng(42.68514249334382, -73.85710670922849)
      },
      beta : {
           icon : 'images/beta.png'
           ,geo : new google.maps.LatLng(40.739995,-73.983642)
+          ,selection : new google.maps.LatLng(40.73713352576095, -74.01007785205084)
      },
      gamma : {
           icon : 'images/gamma.png'
           ,geo : new google.maps.LatLng(40.913096,-73.123586)
+          ,selection : new google.maps.LatLng(40.912771686314684, -73.14942103723148)
      },
      delta : {
           icon : 'images/delta.png'
           ,geo : new google.maps.LatLng(43.000809,-78.78897)
+          ,selection : new google.maps.LatLng(43.000746228202786, -78.81343174621583)
      },
      epsilon : {
         	icon : 'images/epsilon.png'
         	,geo : new google.maps.LatLng(40.694074, -73.986932)
+          ,selection : new google.maps.LatLng(40.694366552831674, -74.01087463383305)
           ,content : '<div class="chapter-marker" id="epsilon-marker"><img id="elogo-1" src="images/nyupoly_first.png" /><img id="elogo-2" class="pulse-rotate" src="images/nyupoly_circle_1px.png" /><img id="elogo-3" src="images/nyupoly_last.png" /></div>'
      },
      zeta : {
           icon : 'images/zeta.png'
           ,geo : new google.maps.LatLng(42.088828,-75.966972)
+          ,selection : new google.maps.LatLng(42.089273867045165, -75.99581111132812)
      },
      eta : {
           icon : 'images/eta.png'
           ,geo : new google.maps.LatLng(43.0383461,-76.134901)
+          ,selection : new google.maps.LatLng(43.03803243218906, -76.16065020654298)
      },
      theta : {
           icon : 'images/theta.png'
           ,geo : new google.maps.LatLng(40.744323,-74.025868)
+          ,selection : new google.maps.LatLng(40.74341260351704, -74.05281883618166)
      },
      iota : {
           icon : 'images/iota.png'
           ,geo : new google.maps.LatLng(40.4846831,-74.4366424)
+          ,selection : new google.maps.LatLng(40.48246350702227, -74.46007417795406)
      }
 }

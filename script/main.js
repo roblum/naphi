4b770b90f863099b6bf71f5751d582738d931b72
Robert <lum.robert@gmail.com>
2014-10-13 22:43:53 -0400

    added random array function by google for gallery images

--- script/main.js
+++ script/main.js
@@ -130,11 +130,19 @@ function initMarkers(){
         }
     }
 
-    function carouselSlider(){
+                            //+ Jonas Raoni Soares Silva
+                            //@ http://jsfromhell.com/array/shuffle [v1.0]
+                            function shuffle(o){ //v1.0
+                                for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+                                return o;
+                            };
 
-        for (var i=0; i<galleryImages.length; i++){
+    function carouselSlider(){
+        var shuffledImages = shuffle(galleryImages);
+
+        for (var i=0; i<shuffledImages.length; i++){
             var markup = '<div class="item"><img class="lazyOwl" data-src="images/gallery_batch/' + 
-                        galleryImages[i]
+                        shuffledImages[i]
                         + '" alt="Lazy Owl Image"></div>';
 
                 $(markup).appendTo('#lazy-load');

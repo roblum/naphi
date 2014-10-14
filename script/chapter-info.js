40ff03eef3f6a7b486e0af14bd2a4a2757251e82
Robert <lum.robert@gmail.com>
2014-10-14 00:15:05 -0400

    removed icons; not being used. added webmaster geolocation

--- script/chapter-info.js
+++ script/chapter-info.js
@@ -1,56 +1,51 @@
 var chapterInfo = {
 	alpha : {
-          icon : 'images/alpha.png'
           ,geo : new google.maps.LatLng(42.6850794,-73.826036)
           ,selection : new google.maps.LatLng(42.6850794, -73.85710670922849)
           ,content : '<div class="chapter-marker"><img src="images/naphi_letters.png" /><p>A - ALBANY SUNY</p></div>'
      },
      beta : {
-          icon : 'images/beta.png'
           ,geo : new google.maps.LatLng(40.739995,-73.983642)
           ,selection : new google.maps.LatLng(40.739995, -74.01007785205084)
           ,content : '<div class="chapter-marker"><img src="images/naphi_letters.png" /><p>B - CITY UNIVERSITY OF NEW YORK</p></div>'
      },
      gamma : {
-          icon : 'images/gamma.png'
           ,geo : new google.maps.LatLng(40.913096,-73.123586)
           ,selection : new google.maps.LatLng(40.913096, -73.14942103723148)
           ,content : '<div class="chapter-marker"><img src="images/naphi_letters.png" /><p>&#915; - STONYBROOK UNIVERSITY</p></div>'
      },
      delta : {
-          icon : 'images/delta.png'
           ,geo : new google.maps.LatLng(43.000809,-78.78897)
           ,selection : new google.maps.LatLng(43.000809, -78.81343174621583)
           ,content : '<div class="chapter-marker"><img src="images/naphi_letters.png" /><p>&#916; - UNIVERSITY AT BUFFAO</p></div>'
      },
      epsilon : {
-        	icon : 'images/epsilon.png'
         	,geo : new google.maps.LatLng(40.694074, -73.986932)
           ,selection : new google.maps.LatLng(40.694074, -74.01087463383305)
           ,content : '<div class="chapter-marker" id="epsilon-marker"><img id="elogo-1" src="images/nyupoly_first.png" /><img id="elogo-2" class="pulse-rotate" src="images/nyupoly_circle_1px.png" /><img id="elogo-3" src="images/nyupoly_last.png" /></div>'
      },
      zeta : {
-          icon : 'images/zeta.png'
           ,geo : new google.maps.LatLng(42.088828,-75.966972)
           ,selection : new google.maps.LatLng(42.088828, -75.99581111132812)
           ,content : '<div class="chapter-marker"><img src="images/naphi_letters.png" /><p>Z - BINGHAMTON UNIVERSITY</p></div>'
      },
      eta : {
-          icon : 'images/eta.png'
           ,geo : new google.maps.LatLng(43.0383461,-76.134901)
           ,selection : new google.maps.LatLng(43.0383461, -76.16065020654298)
           ,content : '<div class="chapter-marker"><img src="images/naphi_letters.png" /><p>H - SYRACUSE UNIVERSITY</p></div>'
      },
      theta : {
-          icon : 'images/theta.png'
           ,geo : new google.maps.LatLng(40.744323,-74.025868)
           ,selection : new google.maps.LatLng(40.744323, -74.05281883618166)
           ,content : '<div class="chapter-marker"><img src="images/naphi_letters.png" /><p>&#920; - STEVENS INSTITUTE OF TECHNOLOGY</p></div>'
      },
      iota : {
-          icon : 'images/iota.png'
           ,geo : new google.maps.LatLng(40.4846831,-74.4366424)
           ,selection : new google.maps.LatLng(40.4846831, -74.46007417795406)
           ,content : '<div class="chapter-marker"><img src="images/naphi_letters.png" /><p>I - RUTGERS UNIVERSITY</p></div>'
+     },
+     webmaster : {
+          ,geo : new google.maps.LatLng(-80.76536878423629, 91.47270977015862);
+          ,content : '<div>Webmaster Jalape&#241;o</div>'
      }
 }

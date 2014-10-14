53f4955442776b54ae792680cd9fb45d9a1fed9a
Rob Lum <lum.robert@gmail.com>
2014-10-14 09:58:13 -0400

    starting grunt for this project

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        // uglify: {
+        //     dist: {
+        //         files: {
+        //             'script.min.js': ['chapter-info.js', 'gallery-images.js', 'gmaps-styles.js', 'contact.js', 'main.js']
+        //         }
+        //         ,options: {
+        //             banner: '/* Epsilon Naphi - <%= grunt.template.today() %> */'
+        //         }
+        //     },
+        // },
+        watch: {
+            js : {
+              files: ['script/*.js']
+              ,tasks: ['ftp-deploy']
+            }
+        }
+        ,ftp: grunt.file.readJSON('ftp.json')
+        ,'ftp-deploy': {
+                 build: {
+                   auth: {
+                     host: '<%= ftp.host %>',
+                     port: 21,
+                     authPath : 'ftp.ftppass',
+                     authKey: 'key'
+                   },
+                   src: 'script/',
+                   dest: '/staging/script/',
+                   exclusions: ['path/to/source/folder/**/.DS_Store', 'path/to/source/folder/**/Thumbs.db', 'path/to/dist/tmp']
+                 }
+          }
+     // ,jshint: {
+     //        options: {
+     //            laxcomma: true,
+     //            curly: true,
+     //            latedef: true,
+     //            noempty: true,
+     //            nonbsp: true,
+     //            quotmark: true,
+     //            undef: true,
+     //            unused: true,
+     //            boss: true,
+     //            eqnull: true,
+     //            sub: true,
+     //            devel: true,
+     //            globals: {
+     //                jQuery: true,
+     //                $: true,
+     //                FB: true,
+     //                document: true,
+     //                window: true
+     //            },
+     //            ignores: ['Gruntfile.js', '*.min.js']
+     //        },
+     //        uses_defaults: ['*.js']
+     //    },
+     //    csslint: {
+     //        strict: {
+     //            options: {
+     //                'important': false,
+     //                'empty-rules': 2,
+     //                'zero-units': false,
+     //                'universal-selector': false,
+     //                'ids': false,
+     //                'unique-headings': false,
+     //                'overqualified-elements': false,
+     //                'gradients': 2,
+     //                'compatible-vendor-prefixes': false,
+     //                'box-model': false
+     //            },
+     //            src: ['*.css'],
+     //            'exclude-list': ['*.min.css']
+     //        }
+     //    }
+
+
+    });
+
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-ftp-deploy');
+    // grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['watch']);
+
+
+};
+

8867ccaf2083a872aa97f9768e21277bc1d97d35
Rob Lum <lum.robert@gmail.com>
2014-10-14 10:43:49 -0400

    added fav icon

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="styles/owl.carousel.css" />
     <link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.3.11/slick.css"/>
     <link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin,greek,greek-ext' rel='stylesheet' type='text/css'>
+    <link rel="icon" type="image/png" href="crest.png">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 </head>
 
